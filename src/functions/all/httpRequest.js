291986022ec844d015e34dd7cfbe212c8cc9a7f4
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-05-10 04:19:41 +0700

    httpRequest not tested yet with oter method except GET

---
+++ src/functions/all/httpRequest.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+const _s = require("scramb");
+
+// not tested yet with other method...
+module.exports = async (d) => {
+  const inside = d.code.split("$httpRequest[")[1].split("]")[0];
+  const [url, method = "GET", body, proper, ...insideHeaders] = inside.split(
+    ";"
+  );
+  const headers = {
+    "User-Agent": _s.userAgents().result,
+  };
+
+  if (!url) return d.client.sendMessage(
+      d.msg.key.remoteJid,
+      { text: `\`\`\`❌ [whatscode.js] | url required in $httpRequest!\`\`\`` },
+      { quoted: d.msg }
+    );
+
+
+  for (let head of insideHeaders) {
+    const [headName, ...headValue] = head.split(":");
+    headers[headName] = headValue.join(":");
+  }
+
+  let da = await axios(url, {
+    method: method,
+    headers: headers,
+    data: body,
+  }).catch((err) => {
+    return d.client.sendMessage(
+      d.msg.key.remoteJid,
+      {
+        text: `\`\`\`❌ [whatscode.js] | Cannot interact with url. ERR: ${err}\`\`\``,
+      },
+      { quoted: d.msg }
+    );
+  });
+
+  var res = da.data;
+  var result = proper ? res[proper] : res;
+  return JSON.stringify(result);
+};

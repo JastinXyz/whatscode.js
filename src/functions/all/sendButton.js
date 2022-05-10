d976f2326f2eb9fc0ae7519ec39ba7a4870b06a6
JstnLT <jastinlinggar@gmail.com>
2022-05-10 17:38:42 +0700

    im going to test this soon (2)

--- src/functions/all/sendButton.js
+++ src/functions/all/sendButton.js
@@ -1,25 +1,19 @@
 module.exports = async (d) => {
+  const args = d.args.toString().split(",");
   const inside = d.code.split("$sendButton[")[1].split("]")[0];
-   const [id, dtext] = inside.split(
-    ";"
-  );
-  
-  if(!id || !dtext) return d.client.sendMessage(`\`\`\`❌ [whatscode.js] | Usage: $sendButton[id;text].\`\`\``)
-  
-  return {buttonId: id, buttonText: {displayText: dtext}, type: 1};
+  const [text, foot = "", id, dtext] = inside.split(";");
+
+  if (!text || !id || !dtext)
+    return d.client.sendMessage(
+      `\`\`\`❌ [whatscode.js] | Usage: $sendButton[text;footer (optional);id;text].\`\`\``
+    );
+
+  const buttonMessage = {
+    text: text,
+    buttons: { buttonId: id, buttonText: { displayText: dtext }, type: 1 },
+    footer: foot,
+    headerType: 1,
+  };
+
+  return buttonMessage;
 };
-
-/*const args = d.args.toString().split(",");
-
-  const split = d.code.split("$sendbutton").length - 1;
-  const after = d.code.split("$sendbutton")[split];
-
-  if (after.startsWith("[")) {
-    const id = d.code.split("$sendbutton[")[1].split("]")[0];
-    const dtext = d.code.split("$sendbutton[")[2].split("]")[0];
-
-    return {buttonId: id, buttonText: {displayText: dtext}, type: 1},;
-  } else {
-    console.log('You forgot to put "[" after declared $sendbutton') 
-    return d.client.sendMessage(`\`\`\`❌ [whatscode.js] | You forgot to put "[" after declared $sendbutton.\`\`\``)
-  }*/

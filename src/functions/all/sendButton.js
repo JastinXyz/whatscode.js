6f5fff24989a56cf263785093fde281b0b9639f4
JstnLT <jastinlinggar@gmail.com>
2022-05-10 17:08:17 +0700

    im going to tested this soon

--- src/functions/all/sendButton.js
+++ src/functions/all/sendButton.js
@@ -1,5 +1,15 @@
 module.exports = async (d) => {
-  const args = d.args.toString().split(",");
+  const inside = d.code.split("$sendButton[")[1].split("]")[0];
+   const [id, dtext] = inside.split(
+    ";"
+  );
+  
+  if(!id || !dtext) return d.client.sendMessage(`\`\`\`❌ [whatscode.js] | Usage: $sendButton[id;text].\`\`\``)
+  
+  return {buttonId: id, buttonText: {displayText: dtext}, type: 1};
+};
+
+/*const args = d.args.toString().split(",");
 
   const split = d.code.split("$sendbutton").length - 1;
   const after = d.code.split("$sendbutton")[split];
@@ -12,5 +22,4 @@ module.exports = async (d) => {
   } else {
     console.log('You forgot to put "[" after declared $sendbutton') 
     return d.client.sendMessage(`\`\`\`❌ [whatscode.js] | You forgot to put "[" after declared $sendbutton.\`\`\``)
-  }
-};
+  }*/

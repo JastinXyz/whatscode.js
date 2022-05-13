c0f5de18493a7b80dbbc17c8372d6d1eb113d16b
JstnLT <jastinlinggar@gmail.com>
2022-05-13 09:40:51 +0700

    prettier

--- src/functions/all/groupAcceptInvite.js
+++ src/functions/all/groupAcceptInvite.js
@@ -1,16 +1,17 @@
-module.exports = async(d) => {
-const inside = d.code.split("$groupAcceptInvite[")[1].split("]")[0]
-try {
-let g = inside.split('https://chat.whatsapp.com/')[1]
-await d.client.groupAcceptInvite(g)
-} catch(err) {
-d.isError = true
-return d.client.sendMessage(
-d.msg.key.remoteJid,
-{
-text: `\`\`\`❌ [whatscode.js] | Something error on $groupAcceptInvite: ${err}!\`\`\``,
-},
-{ quoted: d.msg })
-}
-return ""
-}
+module.exports = async (d) => {
+  const inside = d.code.split("$groupAcceptInvite[")[1].split("]")[0];
+  try {
+    let g = inside.split("https://chat.whatsapp.com/")[1];
+    await d.client.groupAcceptInvite(g);
+  } catch (err) {
+    d.isError = true;
+    return d.client.sendMessage(
+      d.msg.key.remoteJid,
+      {
+        text: `\`\`\`❌ [whatscode.js] | Something error on $groupAcceptInvite: ${err}!\`\`\``,
+      },
+      { quoted: d.msg }
+    );
+  }
+  return "";
+};

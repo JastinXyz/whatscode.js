285aab8b1e2daa68f7722086d881f9ee7866e7a1
JstnLT <jastinlinggar@gmail.com>
2022-05-13 09:46:19 +0700

    some changes

--- src/functions/all/groupAcceptInvite.js
+++ src/functions/all/groupAcceptInvite.js
@@ -1,17 +1,27 @@
 module.exports = async (d) => {
-  const inside = d.code.split("$groupAcceptInvite[")[1].split("]")[0];
-  try {
-    let g = inside.split("https://chat.whatsapp.com/")[1];
-    await d.client.groupAcceptInvite(g);
-  } catch (err) {
+  const split = d.code.split("$groupAcceptInvite").length - 1;
+  const after = d.code.split("$groupAcceptInvite")[split];
+
+  if (after.startsWith("[")) {
+    const inside = d.code.split("$groupAcceptInvite[")[1].split("]")[0];
+
+    try {
+      let g = inside.split("https://chat.whatsapp.com/")[1];
+      await d.client.groupAcceptInvite(g);
+    } catch (err) {
+      d.isError = true;
+      return d.client.sendMessage(
+        d.msg.key.remoteJid,
+        {
+          text: `\`\`\`❌ [whatscode.js] | Something error on $groupAcceptInvite: ${err}!\`\`\``,
+        },
+        { quoted: d.msg }
+      );
+    }
+
+    return ""
+  } else {
     d.isError = true;
-    return d.client.sendMessage(
-      d.msg.key.remoteJid,
-      {
-        text: `\`\`\`❌ [whatscode.js] | Something error on $groupAcceptInvite: ${err}!\`\`\``,
-      },
-      { quoted: d.msg }
-    );
+    return d.client.sendMessage(d.msg.key.remoteJid, { text: `\`\`\`❌ [whatscode.js] | Usage: $groupAcceptInvite[invite link]!\`\`\`` }, { quoted: d.msg })
   }
-  return "";
 };

8eddbb92070b9468267cd78072ef918361dd10ba
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-05-19 13:11:36 +0700

    fix onlyIf

--- src/functions/all/onlyIf.js
+++ src/functions/all/onlyIf.js
@@ -1,46 +1,57 @@
 module.exports = async (d) => {
   const split = d.code.split("$onlyIf").length - 1;
-  const inside = d.code.split("$onlyIf[")[split].split("]")[0];
-  const [cond, error = "You can't use this command!"] = inside.split(";");
+  const after = d.code.split("$onlyIf")[split];
 
-  const operators = () => {
-    for (const op of ["<=", ">=", "==", "!=", "<", ">"]) {
-      if (cond.includes(op)) return op;
+  if (after.startsWith("[")) {
+    const inside = d.code.split("$onlyIf[")[1].split("]")[0];
+    const [cond, error = "You can't use this command!"] = inside.split(";");
+
+    const operators = () => {
+      for (const op of ["<=", ">=", "==", "!=", "<", ">"]) {
+        if (cond.includes(op)) return op;
+      }
+    };
+
+    const op = operators();
+    if (!operators) {
+      d.isError = true;
+      return d.error(
+        `❌ WhatscodeError: Usage: $onlyIf[condition;error message (optional)]!`
+      );
     }
-  };
 
-  const op = operators();
-  if (!operators)
+    const fields = cond.split(op);
+
+    let pass = true;
+    if (op === "<=") {
+      if (Number(fields[0]) > Number(fields[1])) pass = false;
+    } else if (op === ">=") {
+      if (Number(fields[0]) < Number(fields[1])) pass = false;
+    } else if (op === "==") {
+      if (fields[0] !== fields[1]) pass = false;
+    } else if (op === "<") {
+      if (Number(fields[0]) >= Number(fields[1])) pass = false;
+    } else if (op === ">") {
+      if (Number(fields[0]) <= Number(fields[1])) pass = false;
+    } else if (op === "!=") {
+      if (fields[0] === fields[1]) pass = false;
+    }
+
+    if (!pass) {
+      d.isError = true;
+
+      return d.client.sendMessage(
+        d.msg.key.remoteJid,
+        { text: error },
+        { quoted: d.msg }
+      );
+    } else {
+      return "";
+    }
+  } else {
+    d.isError = true;
     return d.error(
       `❌ WhatscodeError: Usage: $onlyIf[condition;error message (optional)]!`
     );
-
-  const fields = cond.split(op);
-
-  let pass = true;
-  if (op === "<=") {
-    if (Number(fields[0]) > Number(fields[1])) pass = false;
-  } else if (op === ">=") {
-    if (Number(fields[0]) < Number(fields[1])) pass = false;
-  } else if (op === "==") {
-    if (fields[0] !== fields[1]) pass = false;
-  } else if (op === "<") {
-    if (Number(fields[0]) >= Number(fields[1])) pass = false;
-  } else if (op === ">") {
-    if (Number(fields[0]) <= Number(fields[1])) pass = false;
-  } else if (op === "!=") {
-    if (fields[0] === fields[1]) pass = false;
-  }
-
-  if (!pass) {
-    d.isError = true;
-
-    return d.client.sendMessage(
-      d.msg.key.remoteJid,
-      { text: error },
-      { quoted: d.msg }
-    );
-  } else {
-    return "";
   }
 };

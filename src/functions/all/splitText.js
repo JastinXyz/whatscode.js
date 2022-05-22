c3981c735fdd0f155167dba699f7fbd210b753b2
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-05-22 07:14:08 +0700

    add textSplit & splitText

---
+++ src/functions/all/splitText.js
@@ -0,0 +1,14 @@
+module.exports = async (d) => {
+  const inside = d.inside;
+  if (inside == "") {
+    d.isError = true;
+    d.error("❌ WhatscodeError: Usage: $splitText[position]");
+  } else {
+    if (isNaN(parseInt(inside - 1)) || parseInt(inside - 1) < 0) {
+      d.isError = true;
+      d.error("❌ WhatscodeError: invalid number in $splitText");
+    } else {
+      return d.data.splitText[inside - 1];
+    }
+  }
+};
---
+++ src/functions/all/textSplit.js
@@ -0,0 +1,12 @@
+module.exports = async(d) => {
+  const inside = d.inside
+  if(inside == "") {
+    d.isError = true;
+    d.error('❌ WhatscodeError: Usage: $textSplit[text;splitted by?]')
+  } else {
+    let [text, splitted = ""] = inside.split(";")
+
+    d.data.splitText = text.split(splitted)
+    return ""
+  }
+};

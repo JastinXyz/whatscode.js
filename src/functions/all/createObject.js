ba9e08e8d7682d54c7805556fab5ec238eba967d
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-05-22 08:27:40 +0700

    add createObject & getObjectProperty

---
+++ src/functions/all/createObject.js
@@ -0,0 +1,17 @@
+module.exports = async(d) => {
+  var inside = d.inside;
+  if(inside == "") {
+    d.isError = true;
+    d.error('❌ WhatscodeError: Usage: $createObject[json object]')
+  } else {
+    try {
+      inside = JSON.parse(inside)
+    } catch (err) {
+      d.isError = true;
+      d.error('❌ WhatscodeError: Something error in $createObject: ' + err)
+    }
+
+    d.data.createObject = inside
+    return ""
+  }
+};
---
+++ src/functions/all/getObjectProperty.js
@@ -0,0 +1,9 @@
+module.exports = async (d) => {
+  const inside = d.inside;
+  if (inside == "") {
+    d.isError = true;
+    d.error("❌ WhatscodeError: Usage: $getObjectProperty[property]");
+  } else {
+    return JSON.stringify(eval(`d.data.createObject?.${inside}`))
+  }
+};

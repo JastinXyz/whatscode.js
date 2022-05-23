96a9d97db719ab35155351a6b9009ba68a032370
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-05-24 03:54:21 +0700

    you can just put reply to quote the message

--- src/functions/all/reply.js
+++ src/functions/all/reply.js
@@ -1,12 +1,5 @@
 module.exports = async (d) => {
-  const r = d.code.split("$reply").length - 1;
-  var inside = d.code.split("$reply[")[r];
-
-  if (!inside) {
-    d.isError = true;
-    return d.error(`❌ WhatscodeError: Usage: $reply[text to reply]!`);
-  } else {
-    inside = inside.split("]")[0];
-    return inside;
-  }
+  let inside = d.inside;
+  inside? inside = inside : inside = ""
+  return inside;
 };

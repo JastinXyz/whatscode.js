8a7d4fb0574c9964d7a4ad9766ae603ef667b6b2
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-06-04 18:17:57 +0700

    downloadContentFromUrl is actually download content from url

--- src/functions/all/downloadContentFromUrl.js
+++ src/functions/all/downloadContentFromUrl.js
@@ -4,62 +4,58 @@ module.exports = async (d) => {
   const fs = require("fs");
   const axios = require("axios");
 
-  if(!fs.existsSync('./tmp')) {
-    fs.mkdirSync('./tmp')
+  if (!fs.existsSync("./tmp")) {
+    fs.mkdirSync("./tmp");
   }
 
   if (!inside) {
     d.isError = true;
-    return d.error("❌ WhatscodeError: Usage: $downloadContentFromUrl[url;file name (optional)]");
+    return d.error(
+      "❌ WhatscodeError: Usage: $downloadContentFromUrl[url;file name (optional)]"
+    );
   } else {
-    const [l, fileName = fileNameFromUrl(inside)] = inside.split(";")
-    let check = l.split('/').pop().indexOf('.') > -1;
+    const [l, fileName = fileNameFromUrl(inside)] = inside.split(";");
+    const check = fileName.indexOf('.') > -1
     if (!check) {
       d.isError = true;
       return d.error(
-        "❌ WhatscodeError: Invalid url in $downloadContentFromUrl. Expected dot file extension url. Received " +
-          l
+        "❌ WhatscodeError: Something error in $downloadContentFromUrl: Can't match any file name. Try using filename and extension in it."
       );
     } else {
-      if (!fileName) {
-        d.isError = true;
-        return d.error("❌ WhatscodeError: Something error in $downloadContentFromUrl: Can't match any file name.");
-      } else {
-        async function saveDownloadImage(url) {
-          const response = await axios({
-            method: "get",
-            url: url,
-            responseType: "stream",
-          }).catch((err) => {
-            d.isError = true;
-            d.error(
-              `❌ WhatscodeError: Something error in $downloadContentFromUrl: ${err}`
-            );
-          });
+      async function saveDownloadImage(url) {
+        const response = await axios({
+          method: "get",
+          url: url,
+          responseType: "stream",
+        }).catch((err) => {
+          d.isError = true;
+          d.error(
+            `❌ WhatscodeError: Something error in $downloadContentFromUrl: ${err}`
+          );
+        });
 
-          if (!response) {
-            d.isError = true;
-            return d.error(
-              `❌ WhatscodeError: Something error in $downloadContentFromUrl: Maybe url is incorrect? or the website is down?`
-            );
-          } else {
-            const writer = fs.createWriteStream("./tmp/" + fileName);
-            response.data.pipe(writer);
+        if (!response) {
+          d.isError = true;
+          return d.error(
+            `❌ WhatscodeError: Something error in $downloadContentFromUrl: Maybe url is incorrect? or the website is down?`
+          );
+        } else {
+          const writer = fs.createWriteStream("./tmp/" + fileName);
+          response.data.pipe(writer);
 
-            return new Promise((resolve, reject) => {
-              writer.on("finish", () => {
-                resolve();
-              });
-              writer.on("error", () => {
-                reject();
-              });
+          return new Promise((resolve, reject) => {
+            writer.on("finish", () => {
+              resolve();
             });
-          }
+            writer.on("error", () => {
+              reject();
+            });
+          });
         }
-
-        const asdf = await saveDownloadImage(inside);
-        return "./tmp/" + fileName;
       }
+
+      const asdf = await saveDownloadImage(inside);
+      return "./tmp/" + fileName;
     }
   }
 };

4e53cc7ea59c1905144ce22f88ac3c78767b48d7
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-06-11 05:45:25 +0700

    stringEmdsWith: fix typo

--- src/functions/all/stringEndsWith.js
+++ src/functions/all/stringEndsWith.js
@@ -2,7 +2,7 @@ module.exports = async(d) => {
   let inside = d.inside;
   if(!inside) {
     d.isError = true;
-    return d.error(`❌ WhatscodeError: Usage: $stringStartsWith[full text;starts with text]!`);
+    return d.error(`❌ WhatscodeError: Usage: $stringEndsWith[full text;ends with text]!`);
   } else {
     const [n, i] = inside.split(";");
     return n.endsWith(i)

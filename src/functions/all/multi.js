a602ac1219ec019c0e1757f6938f0591678e313e
JastinXyz <73673322+JastinXyz@users.noreply.github.com>
2022-06-10 12:08:08 +0700

    add multi

---
+++ src/functions/all/multi.js
@@ -0,0 +1,16 @@
+module.exports = async (d) => {
+  const inside = d.inside;
+  if (!inside) {
+    d.isError = true;
+    return d.error(`❌ WhatscodeError: Usage: $multi[number;number;...]!`);
+  } else {
+    const [...i] = inside.split(";");
+
+    if (i.some((n) => isNaN(Number(n)))) {
+      d.isError = true;
+      return d.error(`❌ WhatscodeError: Invalid number in: $multi[${inside}]!`);
+    } else {
+      return i.reduce((x, y) => Number(x) * Number(y));
+    }
+  }
+};

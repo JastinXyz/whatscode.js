eaf4d1a348cb4400a3e7ed235c06b2a655f9b7dc
JstnLT <jastinlinggar@gmail.com>
2022-06-19 11:40:57 +0700

    typo

--- src/functions/all/modulo.js
+++ src/functions/all/modulo.js
@@ -2,13 +2,13 @@ module.exports = async (d) => {
   const inside = d.inside;
   if (!inside) {
     d.isError = true;
-    return d.error(`❌ WhatscodeError: Usage: $sum[number;number;...]!`);
+    return d.error(`❌ WhatscodeError: Usage: $modulo[number;number;...]!`);
   } else {
     const [...i] = inside.split(";");
 
     if (i.some((n) => isNaN(Number(n)))) {
       d.isError = true;
-      return d.error(`❌ WhatscodeError: Invalid number in: $sum[${inside}]!`);
+      return d.error(`❌ WhatscodeError: Invalid number in: $modulo[${inside}]!`);
     } else {
       return i.reduce((x, y) => Number(x) % Number(y));
     }
